95a42994373f1f59553c2794e9e8aeab3d65e786
Shakera Zia <31955946+Shakerazia@users.noreply.github.com>
2022-07-07 14:24:18 +0430

    Add files via upload

---
+++ Multiplication.php
@@ -0,0 +1,18 @@
+<?php 
+ 
+for($i=2;$i<=10;$i++) 
+ { 
+ for($table_counter=1;$table_counter<=10;$table_counter++) 
+ { 
+ printf("%d * %d = %d ",$i,$table_counter,($i*$table_counter)); 
+ echo " "; 
+ echo "<br> "; 
+ if($table_counter==10) 
+ { 
+ echo "<br> "; 
+ } 
+ } 
+} 
+  
+ 
+?>
